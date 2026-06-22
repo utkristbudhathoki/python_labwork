4f761e1d8c6ac18014c9a8cfc1b514333cdcd3ca
utkrist <utkristbudhathoki19@gmail.com>
2026-06-22 21:58:10 +0545

    upto lab 2

--- python_labwork/Hangman_game.py
+++ python_labwork/Hangman_game.py
@@ -1,10 +1,7 @@
 import random
 
-<<<<<<< HEAD
 words = ['python', 'Java', 'Javascript', 'Csharp', 'Dotnet']
-=======
-words = ['python', 'Java', 'Javascript', 'CSharp', 'Dotnet']
->>>>>>> acb7e57 (CSP program)
+
 
 word = random.choice(words)
 
